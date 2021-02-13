b3b444aeeadcb8ecdd86728cf868179271379d61
Jeana Rose <jr.mathis608@gmail.com>
2021-02-13 13:52:23 -0500

    Added subtitle to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Hero = (props) => {
   return (
     <section class="hero is-medium is-link">
-      <div class="hero-body">
+      <div class="hero-body has-text-centered">
         <p class="title">Employee Directory</p>
-        <p class="subtitle">Medium subtitle</p>
+        <p class="subtitle">
+          Click on carrots to filter by heading or use the search box to narrow
+          your results.
+        </p>
       </div>
     </section>
   );
