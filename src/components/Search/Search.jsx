a4536f710f38bce8d274060c34043f4649533cbc
Jeana Rose <jr.mathis608@gmail.com>
2021-02-14 11:09:35 -0500

    Changed Search from a class component to a functional  component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,31 +1,15 @@
-import React, { Component } from "react";
-import Table from "../EmployeeTable/EmployeeTable";
+import React from 'react';
 
-class Search extends Component {
-  state = {
-    searchTerm: "",
-  };
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-  render() {
+const Search = (props) => {
     return (
-      <>
         <input
           name="searchTerm"
           className="input mt-6 mb-6"
           type="text"
           placeholder="Search by Name"
-          onChange={this.handleInputChange}
+          onChange={props.handleInputChange}
         />
-        {this.state.searchTerm ? <h3>Test</h3> : <Table />}
-      </>
     );
-  }
-}
+};
 
 export default Search;
