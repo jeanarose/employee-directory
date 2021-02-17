2bce4992ed5e953b7c19cac7d7eca343217f895b
Jeana Rose <jr.mathis608@gmail.com>
2021-02-17 09:02:39 -0500

    Fixed search functionality and sort

--- src/components/EmployeeTable/EmployeeTable.jsx
+++ src/components/EmployeeTable/EmployeeTable.jsx
@@ -7,7 +7,8 @@ import "./EmployeeTable.css";
 class EmployeeTable extends Component {
   state = {
     employees: [],
-    searchTerm: "",
+    filteredEmployees: [],
+    sort: false,
   };
 
   componentDidMount() {
@@ -18,36 +19,49 @@ class EmployeeTable extends Component {
     return axios
       .get("https://randomuser.me/api/?results=30&nat=us")
       .then((response) => {
-        this.setState({ employees: response.data.results });
+        this.setState({
+          employees: response.data.results,
+          filteredEmployees: response.data.results,
+        });
       });
   };
 
   handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-    // const searchedEmployee = this.state.employees.map((employee) => {
-    //   return employee === this.state.searchTerm;
-    // });
-  };
-
-  handleNameSort = (event) => {
-    console.log("Caret clicked!");
-    const sortedNames = this.state.employees;
-    sortedNames.sort((a, b) => {
-      if (a.name.last < b.name.last) {
-        return a.name.last.localeCompare(b.name.last);
-      } else if (a.name.last > b.name.last) {
-        return a.name.last.localeCompare(b.name.last);
-      }
+    const searchTerm = event.target.value;
+    const filteredEmployees = this.state.employees.filter((employee) => {
+      return employee.name.first
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
     });
-    this.setState({ employees: sortedNames });
-    console.log(sortedNames);
+    this.setState({ filteredEmployees: filteredEmployees });
+  };
+// object.values of each employee would be an array of every value so if I do .join I get one long string and then I can search .includes and it will search every column of the employees
+  handleNameSort = (event) => {
+    this.setState({ sort: !this.state.sort });
+    const compare = (a, b) => {
+      if (this.state.sort) {
+        return a.name.last.localeCompare(b.name.last);
+      } else {
+        return b.name.last.localeCompare(a.name.last);
+      }
+    };
+    const sorted = this.state.employees.sort(compare);
+    this.setState({ filteredEmployees: sorted });
   };
 
   handlePhoneSort = (event) => {};
-  handleEmailSort = (event) => {};
+  handleEmailSort = (event) => {
+    const sortedEmail = this.state.employees;
+    sortedEmail.sort((a, b) => {
+      if (a.email < b.email) {
+        return a.email.localeCompare(b.email);
+      } else if (a.email > b.email) {
+        return a.email.localeCompare(b.email);
+      }
+    });
+    this.setState({ employees: sortedEmail });
+  };
   handleDOBSort = (event) => {
-    console.log("Caret clicked!");
     const sortedDOB = this.state.employees;
     sortedDOB.sort((a, b) => {
       if (a.dob.date < b.dob.date) {
@@ -107,13 +121,9 @@ class EmployeeTable extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.searchTerm === ""
-              ? this.state.employees.map((employee) => (
-                  <EmployeeRows {...employee} key={employee.id.value} />
-                ))
-              : this.state.employees.filter((employee) => {
-                  <EmployeeRows />;
-                })}
+            {this.state.filteredEmployees.map((employee) => (
+              <EmployeeRows {...employee} key={employee.id.value} />
+            ))}
           </tbody>
         </table>
       </>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,7 +4,6 @@ const Search = (props) => {
     return (
         <input
           name="searchTerm"
-          value={props.value}
           className="input mt-6 mb-6"
           type="text"
           placeholder="Search by Name"
